70c614ec703fc2fd9feb837ded4df2f769e1dbe1
RealSocius <mineplayer300guy@gmail.com>
2021-08-21 06:31:30 +0200

    v1.1

---
+++ background.js
@@ -0,0 +1,29 @@
+if (
+  document.URL.includes("anicloud.io/anime/stream") &&
+  document.URL.includes("episode")
+) {
+  $("img[data-lang-key='3']")[0].click();
+  $("[data-lang-key='3'] a.watchEpisode").click();
+
+  let a = document.createElement("a");
+  a.href = $(
+    "[data-lang-key='3'] a.watchEpisode i.Streamtape"
+  )[0].parentElement.href;
+  a.innerHTML = "Anschauen";
+  a.setAttribute("style", "all: unset; cursor: pointer;");
+  $(".changeLanguageBox")[0].appendChild(a);
+}
+
+if (
+  document.URL.includes("strtapeadblock") ||
+  document.URL.includes("streamtape")
+) {
+  $(document).ready(function () {
+    let file_path = $("#videolink")[0].innerHTML + "&dl=1";
+    file_path = file_path.replaceAll("&amp;", "&");
+    console.log(file_path);
+    if (confirm("Download?")) {
+      saveAs(file_path);
+    }
+  });
+}
